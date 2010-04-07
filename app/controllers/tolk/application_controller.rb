4bafade21672325103129b001c135ea59995fca0
Pratik Naik <pratiknaik@gmail.com>
2010-04-07 19:37:42 +0100

    Implement authentication mechanism

--- app/controllers/tolk/application_controller.rb
+++ app/controllers/tolk/application_controller.rb
@@ -2,5 +2,12 @@ module Tolk
   class ApplicationController < ActionController::Base
     helper :all
     protect_from_forgery
+
+    cattr_accessor :authenticator
+    before_filter :authenticate
+
+    def authenticate
+      self.authenticator.bind(self).call if self.authenticator && self.authenticator.respond_to?(:call)
+    end
   end
 end
