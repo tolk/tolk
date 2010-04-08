888be55a6f1ea5f37d29e3042fdc84b16cd1e979
Pratik Naik <pratiknaik@gmail.com>
2010-04-08 13:56:16 +0100

    Get rid of Translations controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ ActionController::Routing::Routes.draw do |map|
   map.namespace('tolk') do |tolk|
     tolk.root :controller => 'locales'
     tolk.resources :locales, :member => {:all => :get}
-    tolk.resources :translations
   end
 end
