6b69bc5a671c85eb1b791d020e36108dc8db66a2
Pratik Naik <pratiknaik@gmail.com>
2010-04-09 22:02:21 +0100

    Vendor will_paginate

--- config/environment.rb
+++ config/environment.rb
@@ -10,6 +10,4 @@ Rails::Initializer.run do |config|
     :session_key => '_tolk_session',
     :secret      => 'f2d72b63242db79df080031c60159a419981cc6c066e961405c1a86c5c38ba56c960d6de171dc4cf985f1544c00466400abf0aac2ce1cbdb726f6127d304fb07'
   }
-
-  config.gem 'will_paginate', '~> 2.3.12'
 end
--- init.rb
+++ init.rb
@@ -1,4 +1,6 @@
 Mime::Type.register_alias "text/yaml", :yml
 
-gem 'will_paginate', '~> 2.3.12'
-require 'will_paginate'
+will_paginate = File.join(File.dirname(__FILE__), 'vendor/plugins/will_paginate')
+$: << File.join(will_paginate, 'lib')
+
+require $: << File.join(will_paginate, 'init')
