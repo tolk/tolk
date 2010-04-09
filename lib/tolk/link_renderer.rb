de285c91aa6b14e7c53f4832c3085324bc74e62c
Pratik Naik <pratiknaik@gmail.com>
2010-04-09 20:57:09 +0100

    Add a custom will paginate renderer to fix html auto escaping related issues

---
+++ lib/tolk/link_renderer.rb
@@ -0,0 +1,23 @@
+module Tolk
+  class LinkRenderer < WillPaginate::LinkRenderer
+    def to_html
+      links = @options[:page_links] ? windowed_links : []
+      # previous/next buttons
+      links.unshift page_link_or_span(@collection.previous_page, 'disabled prev_page', @options[:previous_label])
+      links.push    page_link_or_span(@collection.next_page,     'disabled next_page', @options[:next_label])
+      
+      html = links.join(@options[:separator])
+      @options[:container] ? @template.content_tag(:div, html.html_safe!, html_attributes) : html.html_safe!
+    end
+
+    protected
+
+    def page_link(page, text, attributes = {})
+      @template.link_to text.html_safe!, url_for(page), attributes
+    end
+
+    def page_span(page, text, attributes = {})
+      @template.content_tag :span, text.html_safe!, attributes
+    end
+  end
+end
