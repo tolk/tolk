92ca21d09e8176b52c93adb967bd056eeb3e3916
Pratik Naik <pratiknaik@gmail.com>
2010-04-09 22:07:51 +0100

    Oops. Fix copy/paste fail

--- init.rb
+++ init.rb
@@ -3,4 +3,4 @@ Mime::Type.register_alias "text/yaml", :yml
 will_paginate = File.join(File.dirname(__FILE__), 'vendor/plugins/will_paginate')
 $: << File.join(will_paginate, 'lib')
 
-require $: << File.join(will_paginate, 'init')
+require File.join(will_paginate, 'init')
