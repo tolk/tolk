88802ba08551f59facdcd44c18bcf2b583c1b355
Craig Davey <me@craigdavey.ca>
2010-04-14 18:09:31 -0400

    Condition the add missing indices migration to handle MySQL limitations with TEXT/BLOB columns.

--- db/migrate/20100409194926_add_missing_indices.rb
+++ db/migrate/20100409194926_add_missing_indices.rb
@@ -1,7 +1,13 @@
 class AddMissingIndices < ActiveRecord::Migration
   def self.up
     add_index :tolk_locales, :name, :unique => true
-    add_index :tolk_phrases, :key, :unique => true
+    if connection.class.name == "ActiveRecord::ConnectionAdapters::MysqlAdapter"
+      # Manually create an index on the first 512 bytes because MySQL can’t 
+      # build an unbound index on TEXT/BLOB columns.
+      execute "CREATE INDEX `index_tolk_phrases_on_key` ON `tolk_phrases` (`key`(512))"
+    else
+      add_index :tolk_phrases, :key, :unique => true
+    end
     add_index :tolk_translations, [:phrase_id, :locale_id], :unique => true
   end
 
