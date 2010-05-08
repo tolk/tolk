a4dee9669c5e883b0de23a39fb82ec38887c5479
Pratik Naik <pratiknaik@gmail.com>
2010-05-08 13:08:53 +0100

    Sort languages by the name

--- app/helpers/tolk/application_helper.rb
+++ app/helpers/tolk/application_helper.rb
@@ -22,7 +22,7 @@ module Tolk
     def tolk_locale_selection
       existing_locale_names = Tolk::Locale.all.map(&:name)
       pairs = Tolk::Locale::MAPPING.to_a.map(&:reverse)
-      pairs = pairs.sort_by(&:last)
+      pairs = pairs.sort_by(&:first)
       pairs.reject do |pair|
         existing_locale_names.include? pair.last
       end
