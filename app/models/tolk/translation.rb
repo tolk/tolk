ff920096860a3729129ba4bc86f8ae52689592ba
Pratik Naik <pratiknaik@gmail.com>
2010-06-03 12:24:26 +0100

    named_scope -> scope and <% -> <%=

--- app/models/tolk/translation.rb
+++ app/models/tolk/translation.rb
@@ -2,9 +2,7 @@ module Tolk
   class Translation < ActiveRecord::Base
     set_table_name "tolk_translations"
 
-    named_scope :containing_text, lambda { |query|
-      { :conditions => ["tolk_translations.text LIKE ?", "%#{query}%"] }
-    }
+    scope :containing_text, lambda {|query| where("tolk_translations.text LIKE ?", "%#{query}%") }
 
     serialize :text
     validates_presence_of :text, :if => proc {|r| r.primary.blank? && !r.explicit_nil }
