274c5c694ec691178e9552f8d74857b3dbe4df9f
Pratik Naik <pratiknaik@gmail.com>
2010-06-04 01:04:25 +0100

    Use Rails 3 route syntax

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-ActionController::Routing::Routes.draw do |map|
+Rails.application.routes.draw do |map|
   map.namespace('tolk') do |tolk|
     tolk.root :controller => 'locales'
     tolk.resources :locales, :member => {:all => :get, :updated => :get}
