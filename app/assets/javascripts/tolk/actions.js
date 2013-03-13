044cffed25fce67f33d78fb2073bb23adc878b2d
Ferran Basora <fcsonline@gmail.com>
2013-03-13 23:56:31 +0100

    Avoid lose data checking dirty fields

--- app/assets/javascripts/tolk/actions.js
+++ app/assets/javascripts/tolk/actions.js
@@ -10,4 +10,23 @@ $(function () {
     row.find(".translation textarea").val(original.trim());
   });
 
+  // avoid lose data
+  $(".translations textarea").bind("blur", function () {
+    if ($(this).is(".dirty")) {
+      window.onbeforeunload = confirm;
+    }
+  });
+
+  $(".translations textarea").bind("keydown", function () {
+    $(this).addClass("dirty");
+  });
+
+  $("input.save").click(function () {
+    window.onbeforeunload = null;
+  });
+
+  function confirm() {
+    return "You are leaving this page with non-saved data. Are you sure you want to continue?";
+  }
+
 });
