8706bfef5d5c2eb7e6dca61c487409bab926af77
Thomas Darde <thomas@rougecardinal.fr>
2013-03-21 08:06:35 +0100

    Even if deserialiaze symbol could lead to a DOS attack, we need to use it a the moment. Otherwise forms with date are broken on the parent rails app...

--- lib/tolk/engine.rb
+++ lib/tolk/engine.rb
@@ -3,6 +3,8 @@ require 'rails'
 module Tolk
   class Engine < Rails::Engine
     SafeYAML::OPTIONS[:default_mode] = :safe
+    SafeYAML::OPTIONS[:deserialize_symbols] = true
+
     isolate_namespace Tolk
   end
 end
