ff742e50530e6150056cb724441910ad9d207543
Sami Haahtinen <sami@haahtinen.name>
2013-07-11 10:37:59 +0300

    Add asset precompile hooks Fixes #44

--- lib/tolk/engine.rb
+++ lib/tolk/engine.rb
@@ -6,5 +6,11 @@ module Tolk
     SafeYAML::OPTIONS[:deserialize_symbols] = true
 
     isolate_namespace Tolk
+
+    if Rails.version >= '3.1'
+      initializer :assets do |app|
+        app.config.assets.precompile += ['tolk/libraries.js']
+      end
+    end
   end
 end
