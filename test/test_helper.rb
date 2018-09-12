12d381d0660ea58e1eef6706d429c07397a55022
Thomas Darde <thomas@rougecardinal.fr>
2018-09-12 12:09:10 +0200

    Update minitest deprecation

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,7 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require "rails/test_help"
 require "capybara/rails"
-require "mocha/mini_test"
+require "mocha/minitest"
 require 'capybara/poltergeist'
 
 
