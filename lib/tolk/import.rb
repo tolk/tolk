ccbcda9317984d8ac368e4a27de3fc366162f16d
Max Melentiev <melentievm@gmail.com>
2019-03-22 14:09:15 +0530

    Improve import performance

--- lib/tolk/import.rb
+++ lib/tolk/import.rb
@@ -24,27 +24,27 @@ module Tolk
         data = locale.read_locale_file
         return unless data
 
-        phrases = Tolk::Phrase.all
+        phrases_by_key = Tolk::Phrase.all.index_by(&:key)
+        translated_phrase_ids = Set.new(locale.translations.pluck(:phrase_id))
         count = 0
 
         data.each do |key, value|
-          phrase = phrases.detect {|p| p.key == key}
-
-          if phrase
-            translation = locale.translations.new(:text => value, :phrase => phrase)
-            if translation.save
-              count = count + 1
-            elsif translation.errors[:variables].present?
-              puts "[WARN] Key '#{key}' from '#{locale_name}.yml' could not be saved: #{translation.errors[:variables].first}"
-            end
-          else
+          phrase = phrases_by_key[key]
+          unless phrase
             puts "[ERROR] Key '#{key}' was found in '#{locale_name}.yml' but #{Tolk::Locale.primary_language_name} translation is missing"
+            next
+          end
+          next if translated_phrase_ids.include?(phrase.id)
+          translation = locale.translations.new(:text => value, :phrase => phrase)
+          if translation.save
+            count = count + 1
+          elsif translation.errors[:variables].present?
+            puts "[WARN] Key '#{key}' from '#{locale_name}.yml' could not be saved: #{translation.errors[:variables].first}"
           end
         end
 
         puts "[INFO] Imported #{count} keys from #{locale_name}.yml"
       end
-
     end
 
     def read_locale_file
